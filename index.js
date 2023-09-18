12f2d152cf318a40fb9124aa7e58bfe7e91df581
joshua chivile <joshuachivile@gmail.com>
2023-09-18 09:54:09 +0300

    updates dom with js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Josh is the champion'
+
+newHeader.append();
